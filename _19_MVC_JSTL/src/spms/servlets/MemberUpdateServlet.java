76fcc6c3a046aa47b95f5cf8e2ce451a703a4cfb
leeyou31 <81919273+leeyou31@users.noreply.github.com>
2021-10-18 17:14:48 +0900

    Add files via upload
    oct 18th

---
+++ _19_MVC_JSTL/src/spms/servlets/MemberUpdateServlet.java
@@ -0,0 +1,143 @@
+package spms.servlets;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.vo.Member;
+
+/**
+ * Servlet implementation class MemberUpdateServlet
+ */
+@WebServlet("/member/update")
+public class MemberUpdateServlet extends HttpServlet {
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String no = request.getParameter("no");
+		String sqlSelect = "SELECT * FROM MEMBERS WHERE MNO = " + no;
+		
+
+		//서블릿 컨텍스트 객체 생성
+		ServletContext sc = this.getServletContext();
+		
+		//컨텍스트 초기화 매개변수 꺼내 쓰기 -> sc.getIninParameter메소드로 접근
+//		String driver = sc.getInitParameter("driver");
+//		String url = sc.getInitParameter("url");
+//		String id = sc.getInitParameter("username");
+//		String pwd = sc.getInitParameter("password");
+		
+		try {
+			//mysql 제어 객체 로딩
+//			Class.forName(driver);
+//			conn = DriverManager.getConnection(url, id, pwd);
+			//servletcontext 데이터 보관소에 저장되어 있는 Connection 객체를 꺼내 씀
+			conn = (Connection)sc.getAttribute("conn");
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sqlSelect);
+			if(rs.next()) {
+				request.setAttribute("member", new Member()
+															.setNo(rs.getInt("mno"))
+															.setEmail(rs.getString("email"))
+															.setName(rs.getString("mname"))
+															.setCreateDate(rs.getDate("cre_date")));
+			} else {
+				throw new Exception("해당 번호의 회원을 찾을 수 없습니다.");
+			}
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/member/MemberUpdateForm.jsp");
+			rd.forward(request, response);
+//			response.setContentType("text/html;charset=UTF-8");
+//			PrintWriter out = response.getWriter();
+//			out.println("<html><head><title>회원정보</title></head>");
+//			out.println("<body><h1>회원정보</h1>");
+//			out.println("<form action='update' method='post'>");
+//			out.println("번호 : <input type='text' name='no' value='" + no + "' readonly> <br>");
+//			out.println("이름 : <input type='text' name='name' value='" + rs.getNString("mname") + "'> <br>");
+//			out.println("이메일 : <input type='text' name='email' value='" + rs.getNString("email") + "'> <br>");
+//			out.println("가입일 : " + rs.getDate("cre_date") + "<br>");
+//			out.println("<input type='submit' value='저장'>");
+//			out.println("<input type='button' value='취소' onclick='location.href=\"list\"'>");
+//			out.println("</form></body></html>");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//request.setCharacterEncoding("UTF-8");
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		String sqlUpdate = "UPDATE MEMBERS SET EMAIL=?, MNAME=?, MOD_DATE=NOW() WHERE MNO=?";
+		
+		//서블릿 컨텍스트 객체 생성
+		ServletContext sc = this.getServletContext();
+		
+		String driver = sc.getInitParameter("driver");
+		String url = sc.getInitParameter("url");
+		String id = sc.getInitParameter("username");
+		String pwd = sc.getInitParameter("password");
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, id, pwd);
+			stmt = conn.prepareStatement(sqlUpdate);
+			stmt.setString(1, request.getParameter("email"));
+			stmt.setString(2, request.getParameter("name"));
+			stmt.setInt(3, Integer.parseInt(request.getParameter("no")));
+			stmt.executeUpdate();
+			
+			response.sendRedirect("list");
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {			
+			try {
+				if(stmt != null) {
+					stmt.close();
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
